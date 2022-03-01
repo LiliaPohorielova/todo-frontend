5965395ef3df3533e5da209da16954d7f1c32212
Liliia <liliia.pohorielova@nure.ua>
2022-03-01 18:16:43 +0200

    Add Category View To Left Menu

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  // @ts-ignore
+  categories: Category[]
 
-  constructor() { }
-
-  ngOnInit(): void {
+  //Dependency Injection With Constructor
+  constructor(private dataHandler: DataHandlerService) {
   }
 
+  //Метод вызывается после создания данного Компонента;
+  ngOnInit() {
+    this.categories = this.dataHandler.getCategories();
+  }
 }
