b6b7ef797474d3bc02533e536135a78725addab7
Liliia <liliia.pohorielova@nure.ua>
2022-03-02 23:49:13 +0200

    Add Mat-table For Tasks

--- src/app/views/tasks/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DataHandlerService} from "../../../service/data-handler.service";
 import {Task} from "../../../model/Task";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-tasks',
@@ -8,6 +9,10 @@ import {Task} from "../../../model/Task";
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+  //поля таблицы (названия колонок)
+  public displayedColumns: string[] = ['color','id','title','date','priority','category'];
+  // @ts-ignore
+  public dataSource: MatTableDataSource<Task>; //контейнер - источник данных для таблицы
 
   // @ts-ignore
   tasks: Task[];
@@ -17,9 +22,27 @@ export class TasksComponent implements OnInit {
   ngOnInit() {
     // @ts-ignore
     this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+    this.dataSource = new MatTableDataSource();
+    this.refreshTable();
   }
 
   toggleTaskCompleted(task: Task) {
     task.completed = !task.completed;
+  }
+
+  //в зависимости от статуса - вернуть цвет
+  public getPriorityColor(task: Task) {
+    if (task.completed) {
+      return "#F8F9FA";
+    }
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+    return '#fff';
+  }
+
+  //показывает задачи учитывая поиск, фильтр, категории
+  private refreshTable() {
+    this.dataSource.data = this.tasks; //обновить источник данных для таблицы
   }
 }
